8609982387a684e85ba4e14143270f25d125af3f
王志凌 <ri0806449@yahoo.com.tw>
2020-08-04 10:21:37 +0800

    another try

--- hello.php
+++ hello.php
@@ -1,1 +1,11 @@
-vvvhi man
+<link rel="shortcut icon" href=".ico">vvvhi man
+o=f=<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
