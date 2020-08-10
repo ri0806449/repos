be099b6d9fd6b88c52ff5a1d8600626d720cad9d
王志凌 <ri0806449@yahoo.com.tw>
2020-08-10 16:24:59 +0800

    將驗證表單規則建立一個檔案儲存於config資料夾裡

--- CIpro/application/controllers/Register.php
+++ CIpro/application/controllers/Register.php
@@ -16,75 +16,11 @@ class Register extends CI_Controller
 		//表單驗證專用
 		$this->load->library('form_validation');
 		$data['title'] = "CI實作會員系統";
-		//設定表單規則
-		$config = array
-		(
-			array
-			(
-				'field' => 'username',//帳號的value：（需要去）view的對應輸入input去set_values()
-				'label' => '帳號',//出現錯誤訊息時的該欄位稱呼
-				'rules' => 'trim|required|min_length[8]|max_length[16]|is_unique[user.username]',//驗證規則（trim會去掉前後之空白）
-				'errors' => //錯誤時應該跳出來的訊息
-				array(
-					'required' => '你有漏喔（帳號）',
-					'is_unique' => '你帳號跟別人重複囉',
-					'min_length' => '你太短了（帳號）',
-					'max_length' => '你太長了！（帳號）'
-				)
-			),
-			array
-			(
-				'field' => 'password',
-				'label' => '密碼',
-				'rules' => 'trim|required|min_length[3]|max_length[16]',
-				'errors' => //錯誤時應該跳出來的訊息
-				array(
-					'required' => '你有漏喔（密碼）',
-					'min_length' => '你太短了（密碼）',
-					'max_length' => '你太長了！（密碼）'
-				)
-			),
-			array
-			(
-				'field' => 'password_retype',
-				'label' => '密碼確認',
-				'rules' => 'trim|required|matches[password]',
-				'errors' => 
-				array(
-					'matches' => '確認密碼與原密碼不相符',
-					'required' => '你有漏喔（密碼確認）',
-					'min_length' => '你太短了（密碼確認）',
-					'max_length' => '你太長了！（密碼確認）'
-				)
-			),
-			array
-			(
-				'field' => 'email',
-				'label' => '信箱',
-				'rules' => 'trim|required|valid_email',
-				'errors' => 
-				array(
-					'valid_email' => '無效的信箱地址',
-					'required' => '你有漏喔（信箱）'
-				)
-			),
-			array
-			(
-				'field' => 'hobby',
-				'label' => '興趣',
-				'rules' => 'trim|required',
-				'errors' => 
-				array(
-					'required' => '你有漏喔（興趣）'
-				)
-			),									
-		);
-		$this->form_validation->set_rules($config);
 		//自己設定的回調函數
 		$this->form_validation->set_rules('username','帳號','callback_username_check');
 
 		//表單驗證的判斷
-		if($this->form_validation->run() == FALSE){
+		if($this->form_validation->run('register') == FALSE){
 			$this->load->view('main/header', $data);
 			$this->load->view('user_register/content',$data);
 			$this->load->view('main/footer',$data);
