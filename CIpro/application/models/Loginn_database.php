19bbe98d5f060cef0d97e3316bfcdbbbf00ee6a4
王志凌 <ri0806449@yahoo.com.tw>
2020-08-11 18:24:30 +0800

    這個寫法很奇怪，如果跑的動記得換個寫法看看

--- CIpro/application/models/Loginn_database.php
+++ CIpro/application/models/Loginn_database.php
@@ -1,11 +1,40 @@
 <?php
-	class Member_model extends CI_Model
+	class Loginn_Database extends CI_Model
 	{
-		public function get_member_data()
+		//插入資料庫裡的註冊資料
+		public function registration_insert($data)
 		{
-			//取得所有會員資料
-			$query = $this->db->get('user');
-			$row = $query->result_array();
-			return $row;
+			//query得出使用者的帳號是否在資料庫中
+			$condition = "username =" . "'" . $data['username'] . "'";
+			$this->db->select('*');
+			$this->db->from('user');
+			$this->db->where($condition);
+			$this->db->limit(1);
+			$query = $this->db->get();
+			if($query->num_row() == 0){
+				//query得出資料庫裡的其他資料
+				$this->db->insert('user',$data);
+				if ($this->db->affected_rows() > 0) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
+
+		//用帳號密碼讀取資料
+		public function login($data){
+			$condition = "username =" . "'" . $data['username'] . "' AND " . "password =" . "'" . $data['password'] . "'";
+			$this->db->select('*');
+			$this->db->form('user');
+			$this->db->where($condition);
+			$this->db->limit(1);
+			$query = $this->db->get();
+			if($query->num_rows() == 1){
+				return $query->result();
+			}
+			else{
+				return false;
+			}
 		}
 	}
