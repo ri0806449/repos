8afdd47979a22eac3355b15c146ad1d0ac96c87f
王志凌 <ri0806449@yahoo.com.tw>
2020-08-11 11:30:38 +0800

    把使用者登入的驗證建立起來！！

--- CIpro/application/controllers/Loginn.php
+++ CIpro/application/controllers/Loginn.php
@@ -4,20 +4,47 @@ class Loginn extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
-		//$this->load->model('loginn_model');
+		$this->load->model('member_model');
 	}
 
 
 	//主頁的相關資訊
 	public function index()
 	{	
-		//載入all.css
-		$this->load->helper('url');
-		//$data['user'] = $this->member_model->get_member_data();
+		//載入all.css與表單輔助函數
+		$this->load->helper(array('form','url'));
+		//載入表單驗證
+		$this->load->library('form_validation');
+
 		$data['title'] = "CI實作會員系統";
-		$this->load->view('main/header', $data);
-		$this->load->view('loginn/content',$data);
-		$this->load->view('main/footer',$data);
+
+		//使用者登入
+		if ($this->form_validation->run('sign_up_user') == FALSE) {
+			$this->load->view('main/header', $data);
+			$this->load->view('loginn/content',$data);
+			$this->load->view('main/footer',$data);
+		}
+		else{
+			$data['user'] = $this->member_model->get_member_data();
+			$this->load->view('main/header',$data);
+			$this->load->view('main/content',$data);
+			$this->load->view('main/footer',$data);
+		};
+
+/*管理者登入晚一點在寫，一起寫太靠北了，debug不完誒靠
+		//管理者登入
+		if($this->form_validation->run('sign_up_admin') == FALSE){
+			$this->load->view('main/header',$data);
+			$this->load->view('loginn/content',$data);
+			$this->load->view('main/footer',$data);
+		}
+		else{
+			$data['user'] = $this->member_model->get_member_data();
+			$this->load->view('main/header',$data);
+			$this->load->view('main/content',$data);
+			$this->load->view('main/footer',$data);
+		};
+*/
 	}
 
 }	
--- CIpro/application/views/loginn/Content.php
+++ CIpro/application/views/loginn/Content.php
@@ -3,25 +3,31 @@
       <div class="row">
 
       <!--使用者登入填寫欄位-->
-        <form class="col s6 offset-s3 login">
+      <?php 
+        $attributes = array('class' => 'col s6 offset-s3 login');
+        echo form_open('loginn',$attributes);
+      ?>
+      <form class="col s6 offset-s3 login">
           <div class="row">
             <h4>登入資訊</h4>
               <div class="input-field col s12">
-                <input id="username" type="text" class="validate" autofocus required>
+                <input id="login_user_username" type="text" class="validate" name="login_user_username" value="<?= set_value('login_user_username') ?>" autofocus>
                 <label for="username">帳號</label>
+                <span class="helper-text reg_error" data-error="wrong" data-success=""><?= form_error('login_user_username'); ?></span>
               </div>
             </div>
           <div class="row">
             <div class="input-field col s12">
-              <input id="password" type="password" class="validate" required>
+              <input id="login_user_password" type="password" class="validate" name ="login_user_password" value="<?= set_value('login_user_password')  ?>" >
               <label for="password">密碼</label>
+              <span class="helper-text reg_error" data-error="wrong" data-success=""><?= form_error('login_user_password'); ?></span>
             </div>
           </div>
           <button class="btn waves-effect waves-light btn-large" type="submit" name="action">登入
             <i class="material-icons right">send</i>
           </button>
           <br>
-          <p style="text-align:left;">新使用者？ <a href="register.php">請註冊</a></p>
+          <p style="text-align:left;">新使用者？ <a href="register">請註冊</a></p>
         </form>
 
         <!--管理者登入填寫欄位-->
@@ -29,14 +35,16 @@
           <div class="row">
             <h6>管理者登入資訊</h6>
               <div class="input-field col">
-                <input id="admin_username" type="text" class="validate" required>
+                <input id="login_admin_username" type="text" class="validate" name ="login_admin_username" value="<?= set_value('login_admin_username')  ?>" >
                 <label for="admin_username">帳號</label>
+                <span class="helper-text reg_error" data-error="wrong" data-success=""><?= form_error('login_admin_username'); ?></span>
               </div>
             </div>
           <div class="row">
             <div class="input-field col">
-              <input id="admin_password" type="password" class="validate" required>
+              <input id="login_admin_password" type="password" class="validate" name="login_admin_password" value="<?= set_value('login_admin_password')  ?>" >
               <label for="admin_password">密碼</label>
+              <span class="helper-text reg_error" data-error="wrong" data-success=""><?= form_error('login_admin_password'); ?></span>
             </div>
           </div>
           <button class="btn waves-effect waves-light" type="submit" name="action">登入
