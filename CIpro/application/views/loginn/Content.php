e89fa7366c82c8ebee0cb69e291a9365f092572f
王志凌 <ri0806449@yahoo.com.tw>
2020-08-18 11:48:03 +0800

    將程式碼以header、main、footer的架構完成，以確保footer可以黏在最下方

--- CIpro/application/views/loginn/Content.php
+++ CIpro/application/views/loginn/Content.php
@@ -1,57 +1,67 @@
 <body>
-
-<?php
-  //這兩部分應該就是登出與註冊成功（？）時會跳出的訊息了.....吧
-  if (isset($logout_message)) {
-  echo "<div class='message'>";
-  echo $logout_message;
-  echo "</div>";
-  }
-  ?>
-
-    <div class="container">
-      <div class="row">
-
-      <!--使用者登入填寫欄位-->
-      <?php 
-        $attributes = array('class' => 'col s5 offset-s3 login');
-        echo form_open('user_authentication/user_login_process',$attributes);
-      ?>
-      <?php
-        echo "<div class='error_msg'>";
-        if (isset($error_message)) {
-        echo $error_message;
-        }
-        echo "</div>";
-      ?>
-          <div class="row">
-            <h4>登入資訊</h4>
-                <span class="helper-text reg_error" data-error="wrong" data-success="">            
-                  <?php
-                    if (isset($message_display)) {
-                    echo $message_display;
-                    }
-                  ?>
-                </span>
-              <div class="input-field col s12">
-                <input id="login_user_username" type="text" class="validate" name="login_user_username" value="<?= set_value('login_user_username') ?>" autofocus>
-                <label for="login_user_username">帳號</label>
-                <span class="helper-text reg_error" data-error="wrong" data-success=""><?= form_error('login_user_username'); ?></span>
-              </div>
-            </div>
-          <div class="row">
-            <div class="input-field col s12">
-              <input id="login_user_password" type="password" class="validate" name ="login_user_password" value="<?= set_value('login_user_password')  ?>" >
-              <label for="login_user_password">密碼</label>
-              <span class="helper-text reg_error" data-error="wrong" data-success=""><?= form_error('login_user_password'); ?></span>
-              <a href="<?= base_url() ?>index.php/user_authentication/user_register">忘記密碼？</a>
-            </div>
-          </div>
-          <button class="btn waves-effect waves-light btn-large" type="submit" name="action">登入
-            <i class="material-icons right">send</i>
-          </button>
-          <br>
-          <p style="text-align:left;">新使用者？ <a href="<?= base_url() ?>index.php/user_authentication/user_register">請註冊</a></p>
-        </form>
+  <header>
+    <div class="row">
+      <div class="col s9">
+        <h4 class="center">登入資訊</h4>
       </div>
     </div>
+  </header>
+  <main>
+    <?php
+      //這兩部分應該就是登出與註冊成功（？）時會跳出的訊息了.....吧
+      if (isset($logout_message)) {
+      echo "<div class='message'>";
+      echo $logout_message;
+      echo "</div>";
+      }
+      ?>
+
+        <div class="container">
+          <div class="row">
+
+          <!--使用者登入填寫欄位-->
+          <?php 
+            $attributes = array('class' => 'col s5 offset-s3 login');
+            echo form_open('user_authentication/user_login_process',$attributes);
+          ?>
+          <?php
+            echo "<div class='error_msg'>";
+            if (isset($error_message)) {
+            echo $error_message;
+            }
+            echo "</div>";
+          ?>
+              <div class="row">
+                    <span class="helper-text reg_error" data-error="wrong" data-success="">            
+                      <?php
+                        if (isset($message_display)) {
+                        echo $message_display;
+                        }
+                      ?>
+                    </span>
+                  <div class="input-field col s12">
+                    <input id="login_user_username" type="text" class="validate" name="login_user_username" value="<?= set_value('login_user_username') ?>" autofocus>
+                    <label for="login_user_username">帳號</label>
+                    <span class="helper-text reg_error" data-error="wrong" data-success=""><?= form_error('login_user_username'); ?></span>
+                  </div>
+                </div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <input id="login_user_password" type="password" class="validate" name ="login_user_password" value="<?= set_value('login_user_password')  ?>" >
+                  <label for="login_user_password">密碼</label>
+                  <span class="helper-text reg_error" data-error="wrong" data-success=""><?= form_error('login_user_password'); ?></span>
+                  <a href="<?= base_url() ?>index.php/user_authentication/forgot_password">忘記密碼？</a>
+                </div>
+              </div>
+              <button class="btn waves-effect waves-light btn-large" type="submit" name="action">登入
+                <i class="material-icons right">send</i>
+              </button>
+              <br>
+              <p style="text-align:left;">新使用者？ <a href="<?= base_url() ?>index.php/user_authentication/user_register">請註冊</a></p>
+            </form>
+          </div>
+        </div>
+
+  </main>
+
+
--- CIpro/application/views/user_register/Content.php
+++ CIpro/application/views/user_register/Content.php
@@ -1,82 +1,85 @@
   <body>
-    <div class="row">
-      <!--把<form class="col s12 reg">換成form_open方式打開，產生 action 的時候， 是基於設定文件來產生 URL 的，這使得您的應用在更改 URL 時更具移植性-->
-       <?php 
-          $attributes = array('class'=>'col s12 reg');
-          echo form_open('user_authentication/new_user_registration', $attributes); 
+    <header></header>
+    <main>
+      <div class="row">
+        <!--把<form class="col s12 reg">換成form_open方式打開，產生 action 的時候， 是基於設定文件來產生 URL 的，這使得您的應用在更改 URL 時更具移植性-->
+         <?php 
+            $attributes = array('class'=>'col s8 offset-s2 reg');
+            echo form_open('user_authentication/new_user_registration', $attributes); 
+          ?>
+         <?php
+          //echo驗證錯誤（但不確定為什麼要放在這裡，等畫面出來後再來看看）
+          echo "<div class='error_msg'>";
+          if (isset($error_message)) {
+          echo $error_message;
+          }
+          echo "</div>";
         ?>
-       <?php
-        //echo驗證錯誤（但不確定為什麼要放在這裡，等畫面出來後再來看看）
-        echo "<div class='error_msg'>";
-        if (isset($error_message)) {
-        echo $error_message;
-        }
-        echo "</div>";
-      ?>
-        <fieldset>
-          <legend><h4>註冊資訊</h4></legend>
-          <!--帳號輸入-->
-          <div class="row">
-            <div class="col s10 offset-s1">
-              <div class="input-field col s12">
-                <input id="username" type="text" class="validate" name="username" value="<?= set_value('username'); ?>"><!-- set_value= 的是該欄位的name-->
-                <label for="username">帳號</label>
-                <span class="helper-text reg_error" data-error="wrong" data-success=""><?= form_error('username'); ?></span>
-              </div>
-              <!--密碼輸入-->
-              <div class="input-field col s6">
-                <input id="password" type="text" class="validate" name="password" value="<?= set_value('password'); ?>">
-                <label for="password">密碼</label>
-                <span class="helper-text reg_error" data-error="wrong" data-success=""><?= form_error('password'); ?></span>
-              </div>
-              <!--確認密碼輸入-->
-              <div class="input-field col s6">
-                <input id="password_retype" type="text" class="validate" name="password_retype" value="<?= set_value('password_retype'); ?>">
-                <label for="password_retype">再輸入一次密碼</label>
-                <span class="helper-text reg_error" data-error="wrong" data-success=""><?= form_error('password_retype'); ?></span>
-              </div>
-              <div class="row">
-              <!--信箱輸入-->                
-                <div class="input-field col s6">
-                  <input id="email" type="text" class="validate" name="email" value="<?= set_value('email'); ?>">
-                  <label for="email">信箱</label>
-                  <span class="helper-text reg_error" data-error="wrong" data-success=""><?= form_error('email'); ?></span>
-                </div>
-                <!--性別輸入-->
-                <div class="input-field col s3 offset-s1">
-                  <h6>性別</h6>
-                  <p>
-                    <label>
-                      <input name="gender" type="radio" value="1" checked />
-                      <span>男</span>
-                    </label>
-                  </p>
-                  <p>
-                    <label>
-                      <input name="gender" type="radio" value='0' />
-                      <span>女</span>
-                    </label>
-                  </p>
-                </div>                    
-              </div>                
-              <!--興趣輸入-->
-              <div class="row">
+          <fieldset>
+            <legend><h4>註冊資訊</h4></legend>
+            <!--帳號輸入-->
+            <div class="row">
+              <div class="col s10 offset-s1">
                 <div class="input-field col s12">
-                  <textarea id="hobby" class="materialize-textarea" name="hobby" value="<?= set_value('hobby'); ?>"></textarea>
-                  <label for="hobby">興趣</label>
-                  <span class="helper-text reg_error" data-error="wrong" data-success=""><?= form_error('hobby'); ?></span>
+                  <input id="username" type="text" class="validate" name="username" value="<?= set_value('username'); ?>"><!-- set_value= 的是該欄位的name-->
+                  <label for="username">帳號</label>
+                  <span class="helper-text reg_error" data-error="wrong" data-success=""><?= form_error('username'); ?></span>
                 </div>
+                <!--密碼輸入-->
+                <div class="input-field col s6">
+                  <input id="password" type="text" class="validate" name="password" value="<?= set_value('password'); ?>">
+                  <label for="password">密碼</label>
+                  <span class="helper-text reg_error" data-error="wrong" data-success=""><?= form_error('password'); ?></span>
+                </div>
+                <!--確認密碼輸入-->
+                <div class="input-field col s6">
+                  <input id="password_retype" type="text" class="validate" name="password_retype" value="<?= set_value('password_retype'); ?>">
+                  <label for="password_retype">再輸入一次密碼</label>
+                  <span class="helper-text reg_error" data-error="wrong" data-success=""><?= form_error('password_retype'); ?></span>
+                </div>
+                <div class="row">
+                <!--信箱輸入-->                
+                  <div class="input-field col s6">
+                    <input id="email" type="text" class="validate" name="email" value="<?= set_value('email'); ?>">
+                    <label for="email">信箱</label>
+                    <span class="helper-text reg_error" data-error="wrong" data-success=""><?= form_error('email'); ?></span>
+                  </div>
+                  <!--性別輸入-->
+                  <div class="input-field col s3 offset-s1">
+                    <h6>性別</h6>
+                    <p>
+                      <label>
+                        <input name="gender" type="radio" value="1" checked />
+                        <span>男</span>
+                      </label>
+                    </p>
+                    <p>
+                      <label>
+                        <input name="gender" type="radio" value='0' />
+                        <span>女</span>
+                      </label>
+                    </p>
+                  </div>                    
+                </div>                
+                <!--興趣輸入-->
+                <div class="row">
+                  <div class="input-field col s12">
+                    <textarea id="hobby" class="materialize-textarea" name="hobby" value="<?= set_value('hobby'); ?>"></textarea>
+                    <label for="hobby">興趣</label>
+                    <span class="helper-text reg_error" data-error="wrong" data-success=""><?= form_error('hobby'); ?></span>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col s12 offset-s4">
+                    <button class="btn-large waves-effect waves-light" type="submit" value="submit">
+                      送出
+                      <i class="material-icons right">check</i>
+                    </button>
+                  </div>
+                </div>              
               </div>
-              <div class="row">
-                <div class="col s12 offset-s4">
-                  <button class="btn-large waves-effect waves-light" type="submit" value="submit">
-                    送出
-                    <i class="material-icons right">check</i>
-                  </button>
-                </div>
-              </div>              
             </div>
-          </div>
-        </fieldset>
-      </form>
-    </div>
+          </fieldset>
+        </form>
+      </div>
+    </main>
